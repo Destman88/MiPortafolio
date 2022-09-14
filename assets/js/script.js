911abcfa29fbb730884b7f1fea205fa09be0fa65
Destman88 <derickmaitland@gmail.com>
2022-09-14 01:06:16 -0600

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,30 @@
+const fecha = new Date();
+const year = fecha.getUTCFullYear();
+document.getElementById("anhoDinamico").innerHTML = year;
+
+//
+function isElementInViewport(el) {
+  let rect = el.getBoundingClientRect();
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <=
+      (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+}
+
+let items = document.querySelectorAll(".timeline li");
+
+// code for the isElementInViewport function
+
+function callbackFunc() {
+  for (let i = 0; i < items.length; i++) {
+    if (isElementInViewport(items[i])) {
+      items[i].classList.add("in-view");
+    }
+  }
+}
+
+window.addEventListener("load", callbackFunc);
+window.addEventListener("scroll", callbackFunc);
